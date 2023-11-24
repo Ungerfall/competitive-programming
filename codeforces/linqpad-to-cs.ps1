cc37158014dd1f62a6b0f57165baf42ad83688f4
Leonid Petrov1 <leonid_petrov1@epam.com>
2023-11-24 17:34:53 +0300

    Add script to convert linqpad expression to csharp program

---
+++ codeforces/linqpad-to-cs.ps1
@@ -0,0 +1,18 @@
+# PowerShell Script to Output Text from Stdin Starting From Specific Line
+
+# Initialize a flag to start copying
+$startCopying = $false
+
+# Read each line from the standard input
+$Input | ForEach-Object {
+    # Check if the current line contains 'namespace Program'
+    if ($_ -match "namespace Program") {
+        # Set the flag to start copying
+        $startCopying = $true
+    }
+
+    # Output the line if the flag is set
+    if ($startCopying) {
+        $_
+    }
+}
