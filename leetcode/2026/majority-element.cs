37d0275478873b2a3e01a0bb0733fed03b0ce446
Leonid Petrov <ungerfall@gmail.com>
2026-09-20 01:24:21 +0300

    Solve majority element
    Boyer-Moore majority voting algoritm

---
+++ leetcode/2026/majority-element.cs
@@ -0,0 +1,42 @@
+﻿#!/usr/bin/env dotnet run
+
+#:include utils.cs
+
+using System.Text;
+using System.Text.Json.Serialization;
+
+Solution s = new();
+int[] nums = "[2,2,1,1,1,2,2]".DeserializeToArray();
+var result = s.MajorityElement(nums);
+string print = result.ToString();
+Console.WriteLine(print);
+
+public class Solution
+{
+    public int MajorityElement(int[] nums)
+    {
+        int candidate = -1;
+        int votes = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (votes == 0)
+            {
+                candidate = nums[i];
+                votes = 1;
+            }
+            else
+            {
+                if (candidate == nums[i])
+                {
+                    votes++;
+                }
+                else
+                {
+                    votes--;
+                }
+            }
+        }
+
+        return candidate;
+    }
+}
