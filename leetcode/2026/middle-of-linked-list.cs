fc55a70ad5b0566d5091e09d22ab9a2c5a22e91f
Leonid Petrov <ungerfall@gmail.com>
2026-09-23 04:02:16 +0300

    Solve middle of the linked list

---
+++ leetcode/2026/middle-of-linked-list.cs
@@ -0,0 +1,31 @@
+﻿#!/usr/bin/env dotnet run
+
+#:include utils.cs
+
+using System.Text;
+using System.Text.Json.Serialization;
+
+Solution s = new();
+ListNode? head = "[1,2,3,4,5,6]".DeserializeToLinkedList();
+var result = s.MiddleNode(head);
+result.Print(Console.Out);
+
+public class Solution {
+    public ListNode MiddleNode(ListNode head) {
+        if (head?.next is null)
+        {
+            return head;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast is not null && fast.next is not null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+}
